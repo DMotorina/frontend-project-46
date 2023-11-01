71feca012a3540f6b1c2332c752b9b93f1f24ec6
DMotorina <lady.renfri@gmail.com>
2023-11-01 14:41:41 +0300

    setting gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,9 @@ const program = new Command();
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>',  'output format')
   .description('Compares two configuration files and shows a difference.');
 
 program.parse();
