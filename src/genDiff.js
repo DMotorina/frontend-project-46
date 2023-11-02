f19a0735554a4a9993ed2e08263f22ee0e447922
DMotorina <lady.renfri@gmail.com>
2023-11-02 14:01:53 +0300

    fix genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,6 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
 export default (filepath1, filepath2) => {
-    const obj1 = JSON.parse(filepath1)
-    const obj2 = JSON.parse(filepath2)
+    const obj1 = JSON.parse(fs.readFileSync(path.resolve(filepath1)))
+    const obj2 = JSON.parse(fs.readFileSync(path.resolve(filepath2)))
 
     const resultObj = Object.assign(obj1, obj2)
 
